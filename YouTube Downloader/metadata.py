eb65d35fae1d821523986fd396a36765debcafa4
Vedant pople <vedantpople4@gmail.com>
2021-12-06 19:53:41 +0530

    Meta Data complete

---
+++ YouTube Downloader/metadata.py
@@ -0,0 +1,15 @@
+from pytube import YouTube
+
+def video(yt):
+    print('Title: ', yt.title)
+    print('Total Length: ', yt.length, ' seconds')
+    print('Total Views: ', yt.views)
+    print('Video Rating: ', round(yt.rating))
+    print('Thumbnail URL: ', yt.thumbnail_url)
+
+
+
+link = "https://www.youtube.com/watch?v=WGQF0XYxE2U"
+yt = YouTube(link)
+
+video(yt)
