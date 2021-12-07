53337dc7b28d6deafb93fbd7c4d2a03fa48db88f
Vedant pople <vedantpople4@gmail.com>
2021-12-07 22:14:31 +0530

    Video Downloader

---
+++ YouTube Downloader/main.py
@@ -0,0 +1,15 @@
+from pytube import YouTube
+
+def video(yt):
+    my_streams = yt.streams.filter(file_extension = 'mp4' , only_video = True)
+    for streams in my_streams:
+        print(f"Video itag : {streams.itag} Resolution : {streams.resolution}")
+
+    input_tag = input("Enter the Tag Value: ")
+    video = yt.streams.get_by_itag(input_tag)
+    video.download()
+    print("Video is downloading as ", yt.title + ".mp")
+
+link = " "
+yt = YouTube(link)
+video(yt)
