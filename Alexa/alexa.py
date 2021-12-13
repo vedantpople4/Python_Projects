11683d869f78e663a2507bdd00d195aec7ab7d09
Vedant pople <vedantpople4@gmail.com>
2021-12-13 23:41:52 +0530

    Import

--- Alexa/alexa.py
+++ Alexa/alexa.py
@@ -5,3 +5,12 @@ import datetime
 import wikipedia
 import pyjokes
 
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voices', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+    
