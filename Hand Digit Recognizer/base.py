b4adc0f889cb6de573263c997fbc0ab4b4dad2d3
Vedant pople <vedantpople4@gmail.com>
2021-12-18 00:44:27 +0530

    Model complete

--- Hand Digit Recognizer/base.py
+++ Hand Digit Recognizer/base.py
@@ -1,5 +1,6 @@
 import keras
 from keras.datasets import mnist
+from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
@@ -16,8 +17,8 @@ x_train = x_train.reshape(x_train.shape[0], 28,28,1)
 x_test = x_test.reshape(x_test.shape[0], 28,28,1)
 input_shape = (28,28,1)
 
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+y_train = keras.utils.np_utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.np_utils.to_categorical(y_test, num_classes)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
@@ -27,3 +28,25 @@ x_test/=255
 print('x_train shape: ', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3,3), activation='relu', input_shape = input_shape))
+model.add(Conv2D(64, (3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
+
+hist = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test))
+print("The model has been successfully trained !")
+
+model.save('mnist.h5')
+print("Model Saved Successfully!")
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print("Test loss: ", score[0])
+print('Test accuracy: ', score[1])
