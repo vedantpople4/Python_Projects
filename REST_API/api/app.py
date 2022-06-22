d1de6cbb738b304f404f454ea2000921457ae7c9
Vedant pople <vedantpople4@gmail.com>
2022-06-23 00:38:39 +0530

    Rest Api

--- REST_API/api/app.py
+++ REST_API/api/app.py
@@ -1,9 +1,31 @@
 from flask import Flask
 
 app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=True)
+    description = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.name} - {self.description}"
+
 @app.route('/')
 def index():
     return 'Hello World!'
 
+@app.route('/drinks')
+def get_drinks():
+    drinks = Drink.query.all()
+    output = []
+    for drink in drinks:
+        drink_data = {'name':drink.name, 'description':drink.description}
+        output.append(drink_data)
+    return {"drinks":output}
+
 if __name__ == '__main__':
-    app.run(use_reloader=True)
+    app.run(debug=True)
